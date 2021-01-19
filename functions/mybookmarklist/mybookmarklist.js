3f20dc81a39e40c50749c65e8b5a707c4adc2242
Muhammad Sahal Qasim <36259752+SheikhSahal@users.noreply.github.com>
2021-01-19 15:40:41 +0500

    client data received to server console

--- functions/mybookmarklist/mybookmarklist.js
+++ functions/mybookmarklist/mybookmarklist.js
@@ -1,6 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server-lambda')
 var faunadb = require('faunadb'),
-  q=faunadb.query;
+  q = faunadb.query;
 
 const typeDefs = gql`
   type Query {
@@ -16,45 +16,44 @@ const typeDefs = gql`
   }
 `
 
-// const authors = [
-//   { id: 1, name: 'Terry Pratchett', married: false },
-//   { id: 2, name: 'Stephen King', married: true },
-//   { id: 3, name: 'JK Rowling', married: false },
-// ]
 
 const resolvers = {
   Query: {
-    bookmarks: async (root,args, context) =>{
-      try{
-        var adminClient = new faunadb.Client({secret:'fnAD_g50O-ACDRrSwACvkqC8rhsA5hlFDkAyJXr'});
+    bookmarks: async (root, args, context) => {
+      try {
+        var adminClient = new faunadb.Client({ secret: 'fnAD_g50O-ACDRrSwACvkqC8rhsA5hlFDkAyJXr' });
         const result = await adminClient.query(
           q.Map(
-            q.Paginate(q.Match(q.Index('bookmark'))),
+            q.Paginate(q.Match(q.Index('Bookmarks'))),
             q.Lambda(x => q.Get(x))
           )
         )
-        console.log(result.data)
-
-        // return[{
-        //   id: 1,
-        //   title:"Temp title" ,
-        //   url: "Temp Url"
-        // }]
-      }catch(err){
+      } catch (err) {
         console.log(err);
       }
-      // return authors;
-    } ,
+    },
   },
-  Mutation:{
-    addBookmark:(_, {title,url}) =>{
-      console.log("=========================");
-        console.log(title,url);
-        return{
-          id: 1,
-          title,
-          url
-        }
+  Mutation: {
+    addBookmark: async (_, { title, url }) => {
+      console.log("====================================")
+      console.log(title,url)
+      try {
+        var adminClient = new faunadb.Client({ secret: 'fnAD_g50O-ACDRrSwACvkqC8rhsA5hlFDkAyJXr' });
+        const result = await adminClient.query(
+          q.Create(
+            q.Collection('Bookmarks'),
+            {
+              data: {
+                title,
+                url
+              }
+            },
+          )
+        )
+        return result.data.data;
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 }
